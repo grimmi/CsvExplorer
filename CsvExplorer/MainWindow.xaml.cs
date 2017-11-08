fe4b0e47d4d4a9449a978c90d5e9bb612f8513df
Arne Lehmann <arne.lehmann@gmx.de>
2017-11-08 21:50:40 +0100

    added icon for open file, loading of csv data works kind of; binding not working yet

--- CsvExplorer/MainWindow.xaml.cs
+++ CsvExplorer/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,12 +19,46 @@ namespace CsvExplorer
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /// </summary>    
     public partial class MainWindow : Window
     {
+        public ICommand OpenFileCommand => new RelayCommand((o) => OpenFile());
+
+        public DataView CsvData { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenFile()
+        {
+            LoadIntoDataGrid(@"c:\temp\test.csv");
+        }
+
+        private void LoadIntoDataGrid(string file)
+        {
+            var data = new DataTable();
+            using (var reader = new StreamReader(file))
+            {
+                var headerLine = reader.ReadLine();
+                var headerNames = headerLine.Split(';');
+                foreach(var header in headerNames)
+                {
+                    data.Columns.Add(new DataColumn(header, typeof(string)));
+                }
+
+                var contentLine = "";
+                var rows = new List<string[]>();
+                while((contentLine = reader.ReadLine()) != null)
+                {
+                    var contentParts = contentLine.Split(';');
+                    data.Rows.Add(contentParts);
+                }
+
+                CsvData = data.DefaultView;
+                csvData.ItemsSource = data.DefaultView;
+            }
         }
     }
 }
---
+++ CsvExplorer/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CsvExplorer
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> Command { get; }
+        private Predicate<object> CanExecutePredicate { get; }
+
+        public RelayCommand(Action<object> command, Predicate<object> canExecute = null)
+        {
+            Command = command;
+            CanExecutePredicate = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return CanExecutePredicate?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Command(parameter);
+        }
+    }
+}
