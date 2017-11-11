c0aa21597aa34407a169648a13f71775fed474fb
Arne Lehmann <arne.lehmann@gmx.de>
2017-11-11 20:49:20 +0100

    implemented floatguesser

---
+++ CsvExplorer/FloatGuesser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CsvExplorer
+{
+    public class FloatGuesser : IDataTypeGuesser
+    {
+        public (double Chance, DataType DataType) Guess(IEnumerable<string> values)
+        {
+            var validFloats = 0;
+            
+            //TODO: check if the numbers use '.' or ',' as decimal separator
+
+            foreach(var value in values)
+            {
+                if (double.TryParse(value, out var d)) validFloats++;
+            }
+
+            return ((double)validFloats / values.Count(), DataType.Float);
+        }
+    }
+}
