eaf16edd1fbc46f7ca703b8bd084eec61beb1ae8
Arne Lehmann <arne.lehmann@gmx.de>
2017-11-12 10:02:04 +0100

    added an ipaddress guesser

---
+++ CsvExplorer/IpAddressGuesser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace CsvExplorer
+{
+    public class IpAddressGuesser : IDataTypeGuesser
+    {
+        public (double Chance, DataType DataType) Guess(IEnumerable<string> values)
+        {
+            var validIps = 0;
+
+            foreach(var value in values)
+            {
+                if (IPAddress.TryParse(value, out var adr)) validIps++;
+            }
+
+            return ((double)validIps / values.Count(), DataType.IpAddress);
+        }
+    }
+}
